ed92c79f7adaac88626c097bef13762cb31d4050
guseyn <guseynism@gmail.com>
2019-10-31 18:06:31 +0400

    fix lint

--- out/e/e-scroll-pagination-box.js
+++ out/e/e-scroll-pagination-box.js
@@ -1,105 +1,50 @@
 'use strict';
-/*
-const { browserified, as } = require('@page-libs/cutie')
-const { CreatedOptions } = browserified(require('@cuties/object'))
-const { ParsedJSON } = browserified(require('@cuties/json'))
-const { StringFromBuffer } = browserified(require('@cuties/buffer'))
-const { ResponseFromAjaxRequest, ResponseBody, ResponseHeaders, ResponseStatusCode } = require('@page-libs/ajax')
-const ShownElement = require('./../async/ShownElement')
-const HiddenElement = require('./../async/HiddenElement')
-const ElementWithMappedObject = require('./../async/ElementWithMappedObject')
-const ParsedElmSelectors = require('./../util/ParsedElmSelectors')
-const PreparedProgressBars = require('./../util/PreparedProgressBars')
-const ShowProgressEvent = require('./../util/ShowProgressEvent')
-const E = require('./../E')
 
-E(
-  'e-scroll-pagination-box',
-  class extends HTMLDivElement {
-    constructor () {
-      super()
-    }
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-    onRender () {
-      this.page = this.getAttribute('data-start-page-number') * 1
-      this.size = this.getAttribute('data-size-number') * 1
-      this.progressBar = new PreparedProgressBars([
-        new ParsedElmSelectors(
-          this.getAttribute('data-progress-bar')
-        ).value()[0]
-      ]).value()[0]
-      this.ajaxIcon = new ParsedElmSelectors(
-        this.getAttribute('data-ajax-icon')
-      ).value()[0]
-      if (this.ajaxIcon) {
-        this.ajaxIcon.style.display = 'none'
-      }
-      this.activate()
-    }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-    activate () {
-      new ShownElement(
-        this.ajaxIcon
-      ).after(
-        new ResponseFromAjaxRequest(
-          new CreatedOptions(
-            'url', this.getAttribute('data-src')
-              .replace('${page}', this.page)
-              .replace('${size}', this.size),
-            'method', 'GET',
-            'headers', new ParsedJSON(
-              this.getAttribute('data-headers') || '{}'
-            ),
-            'progressEvent', new ShowProgressEvent(this.progressBar)
-          )
-        ).as('RESPONSE').after(
-          new ElementWithMappedObject(
-            this,
-            new ParsedJSON(
-              new StringFromBuffer(
-                new ResponseBody(
-                  as('RESPONSE')
-                )
-              )
-            ),
-            'data-response-object-name'
-          ).after(
-            new ElementWithMappedObject(
-              this,
-              new ResponseHeaders(
-                as('RESPONSE')
-              ),
-              'data-response-object-name'
-            ).after(
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-            )
-          )
-          new AppliedActionsOnResponse(
-            this.tagName,
-            this.getAttribute('data-response-object-name') || 'responseObject',
-            new ParsedJSON(
-              new StringFromBuffer(
-                new ResponseBody(
-                  as('RESPONSE')
-                )
-              )
-            ),
-            this.getAttribute('data-response-headers-name') || 'responseHeaders',
-            new ResponseHeaders(
-              as('RESPONSE')
-            ),
-            this.getAttribute('data-response-status-code-name') || 'responseStatusCode',
-            new ResponseStatusCode(
-              as('RESPONSE')
-            ),
-            `hideElms('${this.getAttribute('data-ajax-icon')}');`.concat(
-              this.getAttribute('data-actions-on-response') || ''
-            )
-          )
-        )
-      ).call()
-    }
-  },
-  { extends: 'div' }
-)
-*/
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
+
+function isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _construct(Parent, args, Class) { if (isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
+
+function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[native code]") !== -1; }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var E = require('./../E');
+
+E('e-scroll-pagination-box',
+/*#__PURE__*/
+function (_HTMLTemplateElement) {
+  _inherits(_class, _HTMLTemplateElement);
+
+  function _class() {
+    _classCallCheck(this, _class);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(_class).call(this));
+  }
+
+  _createClass(_class, [{
+    key: "onRender",
+    value: function onRender() {}
+  }]);
+
+  return _class;
+}(_wrapNativeSuper(HTMLTemplateElement)), {
+  "extends": 'template'
+});
