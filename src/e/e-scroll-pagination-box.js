abf941f4e4f549c931f1670982238cf0573649bc
guseyn <guseynism@gmail.com>
2019-10-31 18:06:31 +0400

    fix lint

--- src/e/e-scroll-pagination-box.js
+++ src/e/e-scroll-pagination-box.js
@@ -1,17 +1,5 @@
 'use strict'
 
-const { browserified, as } = require('@page-libs/cutie')
-const { CreatedOptions } = browserified(require('@cuties/object'))
-const { ParsedJSON } = browserified(require('@cuties/json'))
-const { StringFromBuffer } = browserified(require('@cuties/buffer'))
-const { ResponseFromAjaxRequest, ResponseBody, ResponseHeaders, ResponseStatusCode } = require('@page-libs/ajax')
-const JSResponseByHTTPReponseComponents = require('./../async/JSResponseByHTTPReponseComponents')
-const ShownElement = require('./../async/ShownElement')
-const HiddenElement = require('./../async/HiddenElement')
-const ElementWithMappedObject = require('./../async/ElementWithMappedObject')
-const ParsedElmSelectors = require('./../util/ParsedElmSelectors')
-const PreparedProgressBars = require('./../util/PreparedProgressBars')
-const ShowProgressEvent = require('./../util/ShowProgressEvent')
 const E = require('./../E')
 
 E(
@@ -22,7 +10,6 @@ E(
     }
 
     onRender () {}
-
   },
   { extends: 'template' }
 )
