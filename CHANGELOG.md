68798b5973359b32a5fcb0e4714a47ff52e93066
guseyn <guseyn@guseyn.com>
2023-12-01 14:53:09 +0400

    fix

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## Version v1.0.73-1-g92cf101
+
+- c308ce6 1.0.74
+
--- changelog.js
+++ changelog.js
@@ -5,12 +5,12 @@ const { execSync } = require('child_process');
 const previousTag = execSync('git describe --tags $(git rev-list --tags --skip=1 --max-count=1)').toString().trim();
 
 // Get the commit messages and hashes since the last tag
-const commitData = execSync(`git log ${latestTag}..HEAD --pretty=format:"%h %s"`).toString().trim().split('\n');
+const commitData = execSync(`git log ${previousTag}..HEAD --pretty=format:"%h %s"`).toString().trim().split('\n');
 
 // Format the changelog
 const changelog = `# Changelog
 
-## Version ${latestTag}
+## Version ${previousTag}
 
 ${formatCommits(commitData)}
 
