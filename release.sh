5003fe102d67b7a96ef60b50a00460b25921d191
guseyn <guseyn@guseyn.com>
2023-12-06 00:39:20 +0400

    fix script

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ npm version patch --no-git-tag-version
 version=$(jq -r '.version' package.json)
 
 # Get the previus version Git commit
-previousReleaseCommit=$(git log --grep="^release [0-9]\+\.[0-9]\+\.[0-9]\+" --pretty=format:"%H" -n 2 HEAD | tail -n 1)
+previousReleaseCommit=$(git log --grep="^[0-9]\+\.[0-9]\+\.[0-9]\+" --pretty=format:"%H" -n 2 HEAD | tail -n 1)
 echo "Previous Commit Hash: $previousReleaseCommit"
 
 # Get the commit messages and hashes since the last tag
@@ -22,4 +22,4 @@ echo "$changelog" > CHANGELOG.md
 awk -v version="$version" '{gsub(/[0-9]+\.[0-9]+\.[0-9]+/, version)}1' README.md > README.md.tmp
 mv README.md.tmp README.md
 git add --all
-git commit -m "release $version" && git tag -a $version -m "$version"
+git commit -m "$version" && git tag -a $version -m "$version"
