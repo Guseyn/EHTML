1afe209be641d2d57e5c6f213cf6b25333c4c7aa
guseyn <guseyn@guseyn.com>
2023-12-06 00:23:38 +0400

    fix error for attributes

--- src/nodeIsNotForEHTML.js
+++ src/nodeIsNotForEHTML.js
@@ -2,5 +2,5 @@ module.exports = (node) => {
   if (node.parentElement && node.parentElement.closest('[data-no-ehtml="true"]')) {
     return true
   }
-  return node.hasAttribute('data-no-ehtml') && (node.getAttribute('data-no-ehtml') === 'true')
+  return node.hasAttribute && node.hasAttribute('data-no-ehtml') && (node.getAttribute('data-no-ehtml') === 'true')
 }
