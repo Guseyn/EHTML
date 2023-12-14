40a6156dd9c84a339aa0f80a21f16f59385de364
guseyn <guseyn@guseyn.com>
2023-12-14 17:22:58 +0400

    make clone faster

--- src/clone.js
+++ src/clone.js
@@ -1,11 +1,6 @@
 'use strict'
 
 module.exports = (obj) => {
-  if (window.structuredClone) {
-    return window.structuredClone(
-      obj
-    )
-  }
   return JSON.parse(
     JSON.stringify(obj)
   )
